4c98f4ca06c19273b4fb703cd9808a452f25f491
spinxi <50767516+spinxi@users.noreply.github.com>
2023-04-05 12:38:01 +0400

    Update problem-2.py

--- problem-2.py
+++ problem-2.py
@@ -1,3 +1,18 @@
+# def atobitidan_samobiti(num):
+#     if num == 0:
+#         return '0'
+#     else:
+#         samobiti = ''
+#         while num > 0:
+#             #ვიგებთ ნაშთს
+#             nashti = num % 3
+#             #ვუმატებთ სტრინგად ნაშთს
+#             samobiti += str(nashti)
+#             #რიცხვი გავყოთ სამზე და ახლა ამ დარჩენილზე იგივე წავიდა სანამ 0 არ უდრის
+#             num = num // 3
+#         return samobiti
+# print(atobitidan_samobiti(int(input())))
+
 def atobitidan_samobiti(num):
     if num == 0:
         return '0'
@@ -10,5 +25,13 @@ def atobitidan_samobiti(num):
             samobiti += str(nashti)
             #რიცხვი გავყოთ სამზე და ახლა ამ დარჩენილზე იგივე წავიდა სანამ 0 არ უდრის
             num = num // 3
-        return samobiti
-print(atobitidan_samobiti(int(input())))
+        return samobiti[::-1]
+
+
+
+
+num = int(input())
+
+res = atobitidan_samobiti(num)
+
+print(res)

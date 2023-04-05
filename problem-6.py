690479e9a507348f0d91546b1ee1904b7d782d25
spinxi <50767516+spinxi@users.noreply.github.com>
2023-04-05 13:54:37 +0400

    Update problem-6.py

--- problem-6.py
+++ problem-6.py
@@ -1,14 +1,27 @@
+# def sheamowme(arr, target):
+#     count = 0
+#     meore_arr = []
+#     for num in arr:
+#         #გავიგოთ სხვაობა ტარგეტისა და მოცემულ ელემენტის
+#         #თუ მათი სხვაობა იქნება ერეიში ესეიგი ეგეთი წყვილი არსებობს
+#         #და დავამატოთ ყველა არსებობაზე ქაუნთს
+#         difference  = target - num
+#         if difference in meore_arr:
+#             count += 1
+#         meore_arr.append(num)
+#     return count
+# n = int(input())
+# print(sheamowme(list(map(int, input().split())), int(input())))
+
+
 def sheamowme(arr, target):
     count = 0
-    meore_arr = []
-    for num in arr:
-        #გავიგოთ სხვაობა ტარგეტისა და მოცემულ ელემენტის
-        #თუ მათი სხვაობა იქნება ერეიში ესეიგი ეგეთი წყვილი არსებობს
-        #და დავამატოთ ყველა არსებობაზე ქაუნთს
-        difference  = target - num
-        if difference in meore_arr:
-            count += 1
-        meore_arr.append(num)
+    n = len(arr)
+    for i in range(n):
+        for j in range(i+1, n):
+            if arr[i] + arr[j] == target:
+                count += 1
     return count
 n = int(input())
 print(sheamowme(list(map(int, input().split())), int(input())))
+
